801c4d108eb43bf78d77466cb62570563a830754
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-07-31 23:57:01 +0700

    feat: create we always slide

---
+++ src/components/home/value.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex w-[250px] cursor-pointer flex-col items-center gap-4"
+    :class="!isActive && 'opacity-50'"
+  >
+    <NuxtImg :src="src" class="rounded-lg" />
+    <span class="text-xl font-medium">{{ description }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HomeValueProps } from '~/types/home'
+
+defineProps<HomeValueProps>()
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <UContainer class="flex flex-col text-black md:flex-row">
-    <div class="mt-20">
+    <div class="mt-20 w-full">
       <CommonSearch @search="handleSearch" placeholder="Find opportunities" />
+      <HomeWeAlways :values="values" transition-time="5000" :details="details" />
     </div>
   </UContainer>
 </template>
@@ -10,4 +11,21 @@
 const handleSearch = (searchData) => {
   console.log(searchData)
 }
+
+const values = ref([
+  {
+    src: './value.png',
+    description: 'Stay Humble'
+  },
+  {
+    src: './value.png',
+    description: 'Show Care'
+  },
+  {
+    src: './value.png',
+    description: 'Be Enthusiastic'
+  }
+])
+
+const details = ref(['Lorem 1', 'Lorem 2', 'Lorem 3'])
 </script>
