80174c0c339bb647a3d8c72bf99025902c41907d
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-07-31 23:57:01 +0700

    feat: create we always slide

---
+++ src/components/home/we-always.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col items-center gap-12">
+    <h2>At GDSC-UET, we always</h2>
+    <div class="flex w-full">
+      <div class="mx-[100px] flex grow flex-wrap justify-between">
+        <HomeValue
+          v-for="(value, index) in values"
+          :src="value.src"
+          :description="value.description"
+          :key="value.description"
+          :is-active="index === currentIndex"
+          @click="setIndex(index)"
+        />
+      </div>
+    </div>
+    <span>{{ details[currentIndex] }}</span>
+    <UButton size="xl" class="px-20" :ui="{ rounded: 'rounded-full' }">Join us now</UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HomeWeAlwaysProps } from '~/types/home'
+
+defineProps<HomeWeAlwaysProps>()
+
+const currentIndex = ref(0)
+
+const setIndex = (index: number) => {
+  currentIndex.value = index
+}
+</script>
---
+++ src/types/home/index.ts
@@ -0,0 +1,10 @@
+export type HomeValueProps = {
+  src: string
+  description: string
+  isActive: boolean
+}
+
+export type HomeWeAlwaysProps = {
+  values: HomeValueProps[]
+  details: string[]
+}
