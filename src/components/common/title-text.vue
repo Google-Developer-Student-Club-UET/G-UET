e73b2406efb2c01612ad32480bbbba1a906cc7d5
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-08-02 01:25:19 +0700

    fix: text center

--- src/components/common/title-text.vue
+++ src/components/common/title-text.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-5xl font-bold sm:text-6xl">
+  <h1 class="text-center text-5xl font-bold sm:text-6xl">
     {{ prefix }} <span class="text-primary">{{ text }}</span> {{ suffix }}
   </h1>
 </template>
