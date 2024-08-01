fb33f0594191342956fdd835b827dcbaa70b074e
trung1007 <trungthanhcao.2003@gmail.com>
2024-08-01 23:15:49 +0700

    feat: about us

---
+++ src/components/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col gap-10">
+        <h1 class="text-center text-[60px]"> <span class="text-blue-500">About</span> us</h1>
+        <div class="flex gap-5">
+            <NuxtImg src='/about.png' class="w-1/2 h-72 pr-5" alt="" />
+            <span class="flex w-1/2 p-8 items-center">
+                We are students with a passion for technology, always wanting to bring valuable products to the
+                community.
+            </span>
+        </div>
+        <div class="flex gap-5">
+            <span class="flex w-1/2 p-8 items-center">
+                Not only that, we are also people who are always ready to share, support and help people in the process
+                of
+                studying and learning about technology.
+            </span>
+            <NuxtImg src='/about.png' class="w-1/2 h-72 pr-5" alt="" />
+        </div>
+    </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <UContainer class="flex flex-col text-black"> </UContainer>
+  <UContainer class="flex flex-col text-black md:flex-row">
+    <div class="mt-20 w-full">
+      <CommonSearch @search="handleSearch" placeholder="Find opportunities" />
+      <About />
+      <HomeWeAlways />
+    </div>
+  </UContainer>
 </template>
 
 <script setup lang="ts"></script>
