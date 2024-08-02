6498d2a36f3342ccca61dbf297e865ae5b95caa8
caodung27 <cdnh1234567890@gmail.com>
2024-08-02 07:24:51 +0700

    feat: create footer

---
+++ src/components/common/copy-right.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="text-sm sm:text-base text-gray-600 sm:pt-10 pt-4">
+    © Copyright 2024, All Rights Reserved by Google Deveploper Student Clubs - UET
+  </span>
+</template>
---
+++ src/components/footer/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bottom-0 w-full bg-gray-200 py-8 border-t border-gray-300">
+    <UContainer class="relative flex flex-col">
+      <CommonLogo />
+      <div class="flex flex-col sm:flex-row sm:justify-between w-full space-y-6 sm:space-y-0">
+        <CommonCopyRight />
+        <CommonSocialNavigation />
+      </div>
+    </UContainer>
+  </div>
+</template>
