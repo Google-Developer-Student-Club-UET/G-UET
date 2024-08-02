822cf6b1139ffc09c658a48de74b637987ca3b0a
caodung27 <cdnh1234567890@gmail.com>
2024-08-02 07:24:51 +0700

    feat: create footer

---
+++ src/components/common/social-navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex space-x-6 pt-6">
+    <nuxt-link v-for="button in buttons" :key="button.path" :to="button.href">
+      <UButton
+        color="gray"
+        size="xl"
+        class="border border-gray-300 rounded-full w-[44px] h-[44px] text-gray-600 cursor-pointer"
+      >
+        <template #leading>
+          <NuxtImg :src="button.path" alt="" class="w-5 h-5" />
+        </template>
+      </UButton>
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const buttons = ref([
+  { path: '/facebook.png', href: 'https://facebook.com' },
+  { path: '/linkedin.png', href: 'https://linkedin.com' },
+  { path: '/github.png', href: 'https://github.com' },
+  { path: '/email.png', href: 'mailto:example@example.com' },
+  { path: '/instagram.png', href: 'https://instagram.com' }
+]);
+</script>
