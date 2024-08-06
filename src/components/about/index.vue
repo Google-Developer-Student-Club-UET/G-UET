f5cca50b95f7310661cee186187bc37f875511b0
trung1007 <trungthanhcao.2003@gmail.com>
2024-08-06 21:24:59 +0700

    feat: responsive UI

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="flex flex-col gap-10">
-        <h1 class="text-center text-[60px]"> <span class="text-blue-500">About</span> us</h1>
-        <div class="flex gap-5">
-            <NuxtImg src='/about.png' class="w-1/2 h-72 pr-5" alt="" />
-            <span class="flex w-1/2 p-8 items-center">
-                We are students with a passion for technology, always wanting to bring valuable products to the
-                community.
-            </span>
-        </div>
-        <div class="flex gap-5">
-            <span class="flex w-1/2 p-8 items-center">
-                Not only that, we are also people who are always ready to share, support and help people in the process
-                of
-                studying and learning about technology.
-            </span>
-            <NuxtImg src='/about.png' class="w-1/2 h-72 pr-5" alt="" />
-        </div>
+    <div class="flex flex-col gap-10 justify-center items-center">
+        <h1 class="text-center text-[60px]">About us</h1>
+        <AboutItem v-for="item in items" :imgSrc="item.imgSrc" :decription="item.description"
+            :itemClasses="item.cssClasses" />
     </div>
 </template>
+
+<script setup lang="ts">
+const items = ref([
+    {
+        imgSrc: '/about.png',
+        description: 'We are students with a passion for technology, always wanting to bring valuable products to the community.',
+        cssClasses: "flex-col items-center justify-center sm:flex-row flex"
+    },
+    {
+        imgSrc: '/about.png',
+        description: 'Not only that, we are also people who are always ready to share, support and help people in the process of studying and learning about technology.',
+        cssClasses: "flex-col items-center justify-center sm:flex-row-reverse flex"
+    }
+])
+</script>
