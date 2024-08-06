db96fee4658b37d3b16f4b156ad174dcaf64c253
trung1007 <trungthanhcao.2003@gmail.com>
2024-08-06 22:48:29 +0700

    feat: about us

---
+++ src/components/about/item.vue
@@ -0,0 +1,27 @@
+<template>
+    <div :class="itemClasses">
+        <NuxtImg :src="imgSrc" class="w-1/2 h-72 pr-5" alt="" />
+        <span class="flex w-1/2 p-8 items-center">
+            {{ description }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imgSrc: {
+            type: string,
+            required: true
+        },
+        description: {
+            type: string,
+            required: true
+        },
+        itemClasses: {
+            type: string,
+            required: true
+        }
+    }
+}
+</script>
